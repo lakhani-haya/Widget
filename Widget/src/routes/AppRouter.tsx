38bade43931f2066d178e54292606a70624c8671
Haya Lakhani <haya.lakhani-1@ou.edu>
2026-01-08 23:59:15 -0600

    handlers

--- Widget/src/routes/AppRouter.tsx
+++ Widget/src/routes/AppRouter.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import ClockWidget from "../widgets/ClockWidget";
 import NotesWidget from "../widgets/NotesWidget";
 
 function HostScreen() {
+  const [message, setMessage] = useState("");
+
+  const handleOpenClock = () => {
+    console.log("Open Clock Widget clicked");
+    setMessage("Clock widget action triggered");
+    setTimeout(() => setMessage(""), 2000);
+  };
+
+  const handleOpenNotes = () => {
+    console.log("Open Notes Widget clicked");
+    setMessage("Notes widget action triggered");
+    setTimeout(() => setMessage(""), 2000);
+  };
+
   return (
     <div className="app-shell">
       <h1 className="title">Widget Host</h1>
+      <div className="button-group">
+        <button className="widget-button" onClick={handleOpenClock}>
+          Open Clock Widget
+        </button>
+        <button className="widget-button" onClick={handleOpenNotes}>
+          Open Notes Widget
+        </button>
+      </div>
+      {message && <p className="confirmation-message">{message}</p>}
     </div>
   );
 }
